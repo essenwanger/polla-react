d5316aa3b5e469f8990bcb4b8cf781f7cf8ddb62
Miguel Hpm <miguelhpm@gmail.com>
2018-02-07 20:52:40 -0500

    Agregando Scene base

---
+++ src/scenes/FixtureScene.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { Container, Header, Content, Card, CardItem, Text, Body, H3, Button } from 'native-base';
+
+export default class FixtureScene extends Component {
+
+  onPressLogin(){
+    //Actions.pop();
+  }
+
+  render() {
+    return (
+      <Container>
+        
+      </Container>
+    );
+  }
+}
