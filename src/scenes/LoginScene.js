f2cee2083f1361dd31899db2af1deef2fab8bfd8
essenwanger <essenwanger@gmail.com>
2018-02-08 10:22:45 -0500

    Example Firebase read

--- src/scenes/LoginScene.js
+++ src/scenes/LoginScene.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
 import { Container, Text, Button, View, Icon, H1 } from 'native-base';
-import {StyleSheet} from 'react-native'
+import firebase from 'react-native-firebase';
 
 export default class LoginScene extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: ''
+    };
+    this.onPress = this.onPress.bind(this);
+  }
+
+  onPress(){
+    firebase.database().ref().once('value').then((snapshot)=>{
+      this.setState({
+        name: snapshot.val().name
+      });
+    });
+  }
 
   onPressLogin(){
     Actions.terms();
@@ -17,6 +33,10 @@ export default class LoginScene extends Component {
             <Icon name='logo-googleplus' />
             <Text>Login</Text>
           </Button>
+          <Button block onPress={this.onPress}>
+            <Text>Firebase</Text>
+          </Button>
+          <Text>{this.state.name}</Text>
         </View>
       </Container>
     );
