a1b280e669b8db94403186873c3989888575ff9d
Miguel Hpm <miguelhpm@gmail.com>
2018-04-23 23:45:22 -0500

    nuevo login

--- PollaReact/src/scenes/TermsScene.js
+++ PollaReact/src/scenes/TermsScene.js
@@ -10,9 +10,11 @@ export default class TermsScene extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: props.user,
-      matches: [],
-      positionTable: [],      
+      user: props.user,//Para apuestas adicionales enviar siempre.
+      matches: {},
+      roups: [],
+      positionTable: [], 
+      typeOfBet: props.typeOfBet === undefined ? 'All' : props.typeOfBet,
       presentationMode : props.presentationMode === undefined ? 'Login' : props.presentationMode,
       textButton: props.presentationMode === undefined ? 'Aceptar' : ' Salir'
     };    
@@ -28,12 +30,30 @@ export default class TermsScene extends Component {
   prepareLogin() {
     //preparando matches
     firebase.database().ref('matches/').once('value').then((snapshot)=>{
-      var matches=[];
+      var matches={};
+      var groups=[];
       snapshot.forEach(function(childSnapshot) {
         var item = childSnapshot.val();
-        matches.push(item);
+        var key = childSnapshot.key;
+        if(item.group!=='' && matches[item.group]===undefined){
+          groups.push({group: item.group, percentage: '0'});
+          matches[item.group]=[];
+        }
+        if(item.group==='' && matches[item.round]===undefined){
+          groups.push({group: item.round, percentage: '0'});
+          matches[item.round]=[];
+        }
+        if(item.group!==''){
+          matches[item.group][key]=item;
+        }
+        if(item.group===''){
+          matches[item.round][key]=item;
+        }
       });
-      this.state.matches = matches;
+      this.setState({
+        groups: groups,
+        matches: matches
+      });
     });
 
     //preparando positionTable
@@ -47,37 +67,38 @@ export default class TermsScene extends Component {
   }
 
   onAcceptTerms(){
-    /*
-    if(this.state.presentationMode === 'Login') {
-      this.saveUser();
-    } else {
-      Actions.pop();
-    }*/
-    this.saveUser();
-  }
 
-  saveUser() {
+    var preBet = {
+      profile: this.state.user,//Verificar que exista primero
+      completed: false,
+      payable: false,
+      matches: this.state.matches,
+      groups: this.state.groups,
+      positionTable: this.state.positionTable,
+      date: new Date()
+    };
+
+    var refPreBets = firebase.database().ref('preBets' + this.state.typeOfBet);
+    var refPrePush = refPreBets.push(preBet);
+
     //alta de usuario en /users/{id-google}/
     var userFirebase = {
       profile: this.state.user,
       bets: [
         {
-          status: "Creado",
-          pay: "Pendiente",
-          matches: this.state.matches,
-          positionTable: this.state.positionTable
+          betKey: refPrePush.key,
+          completed: false
         }
       ]
     };
     firebase.database().ref('users/' + this.state.user.userID + '/').set(userFirebase);
 
     //alta del usuario en el ranking /ranking/
-    var rank = {
-      bet: 0,
-      points: 0,
-      profile: this.state.user
+    var subscribed = {
+      profile: this.state.user,
+      count: 1      
     };
-    firebase.database().ref('ranking/' + this.state.user.userID + '/').set(rank);
+    firebase.database().ref('subscribed' + this.state.typeOfBet + '/' + this.state.user.userID + '/').set(subscribed);
 
     //Navegando al dashboard 
     Actions.reset('dashboard', {user: this.state.user});
