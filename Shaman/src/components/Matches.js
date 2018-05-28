dc8ea71ef6ea4ba7c4a05550c9933c90d27c6a49
essenwanger <essenwanger@gmail.com>
2018-05-27 23:47:07 -0500

    UX Post Mundial Match

--- Shaman/src/components/Matches.js
+++ Shaman/src/components/Matches.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Content, Spinner, Card, Container, Header, Body, Title, Button, Icon, Text } from 'native-base';
 import { StyleSheet} from 'react-native';
 import Match from './Match';
+import MatchStaticGroup from './MatchStaticGroup';
+import MatchStaticRound from './MatchStaticRound';
 
 export default class Matches extends Component {
 
@@ -47,9 +49,36 @@ export default class Matches extends Component {
   }
 
   render() {
-    var matchesComponent=this.props.matches.map((item, key) => (
-      <Match key={item.id} data={item} status={this.props.status} onChangeScore={this.onChangeScore}/>
-    ));
+    var matchesComponent=null;
+    if(this.props.status==='closed'){
+      matchesComponent=this.props.matches.map((item, key) => (
+        <Match key={item.id} data={item} onChangeScore={this.onChangeScore}/>
+      ));
+    }else{
+      if(this.props.groupKey.length===1){
+        matchesComponent=this.props.matches.map((item, key) => (
+          <MatchStaticGroup key={item.id} data={item} />
+        ));
+      }else{
+        var matchesStatic = [];
+        this.props.matches.forEach((item, key)=>{
+          matchesStatic.push({
+            team1: item.team1, team2: item.team2,
+            scoreTeam1: item.scoreTeam1, scoreTeam2: item.scoreTeam2,
+            scorePenaltyTeam1: item.scorePenaltyTeam1, scorePenaltyTeam2: item.scorePenaltyTeam2,
+            points: item.points
+          });
+          matchesStatic.push({
+            team1: item.teamSource1, team2: item.teamSource2,
+            scoreTeam1: item.scoreTeamReal1, scoreTeam2: item.scoreTeamReal2,
+            scorePenaltyTeam1: item.scorePenaltyTeamReal1, scorePenaltyTeam2: item.scorePenaltyTeamReal2
+          });
+        });
+        matchesComponent=matchesStatic.map((item, key) => (
+          <MatchStaticRound key={key} data={item} />
+        ));
+      }
+    }
     if(this.props.matches.length===0){
       return (
         <Content padder>
@@ -59,23 +88,25 @@ export default class Matches extends Component {
     }else{
       return (
         <Container>
-          <Header style={styles.header} >
-            <Body>
-              {this.state.active ?
-                (
-                  <Button success block onPress={this.onScore}>
-                    <Icon name='ios-checkmark-circle-outline'/><Text>Guardar Apuesta</Text>
-                  </Button>
-                )
-                :
-                (
-                  <Button success disabled block>
-                    <Icon name='ios-checkmark-circle-outline'/><Text>Guardar Apuesta</Text>
-                  </Button>
-                )
-              }
-            </Body>
-          </Header>
+          {this.props.status==='opened' &&
+            <Header style={styles.header} >
+              <Body>
+                {this.state.active ?
+                  (
+                    <Button success block onPress={this.onScore}>
+                      <Icon name='ios-checkmark-circle-outline'/><Text>Guardar Apuesta</Text>
+                    </Button>
+                  )
+                  :
+                  (
+                    <Button success disabled block>
+                      <Icon name='ios-checkmark-circle-outline'/><Text>Guardar Apuesta</Text>
+                    </Button>
+                  )
+                }
+              </Body>
+            </Header>
+          }
           <Content padder>
             <Card>
               {matchesComponent}
