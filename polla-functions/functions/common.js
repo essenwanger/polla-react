8853c975498e0da3fa69642c7c1651666a76befa
Edison Pérez <edisonaqp@gmail.com>
2018-06-14 02:14:37 -0500

    funciones de soporte

--- polla-functions/functions/common.js
+++ polla-functions/functions/common.js
@@ -12,12 +12,70 @@ exports.listUsers = () => functions.https.onRequest((req, res) => {
 
     betsRef.once('value').then((snapshot) => {
         snapshot.forEach((childSnapshot) => {
-            console.log(childSnapshot.val().profile.email + ',' + childSnapshot.val().profile.name + ',' + childSnapshot.val().completed);
+            console.log(childSnapshot.key + ',' + childSnapshot.val().profile.email + ',' + childSnapshot.val().profile.name + ',' + childSnapshot.val().completed + ',' + childSnapshot.val().payable);
         })
     }).catch(error => {
     this.errorMessage = 'Error - ' + error.message
     });
 
     return res.redirect(303, global.init.db.ref('/matches'));
+    
+});
+
+exports.payable = () => functions.https.onRequest((req, res) => {
+
+    console.log('payable');
+
+    const email = req.query.email;
+    const payable = req.query.payable;
+  
+    
+    var betsRef = global.init.db.ref('/preBetsAll');
+
+    betsRef.once('value').then((snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            var preBet = childSnapshot.val();
+
+            if(preBet.profile.email === email) {
+                global.init.db.ref('/preBetsAll/' + childSnapshot.key).update({
+                    "payable": 'true'
+                }).catch(error => {
+                    this.errorMessage = 'Error - ' + error.message
+                });
+                return true;
+            }
+        })
+    }).catch(error => {
+    this.errorMessage = 'Error - ' + error.message
+    });
+
+    return res.redirect(303, global.init.db.ref('/preBetsAll'));
+    
+});
+
+exports.payableCompleted = () => functions.https.onRequest((req, res) => {
+
+    console.log('payable');
+
+    var betsRef = global.init.db.ref('/preBetsAll');
+
+    betsRef.once('value').then((snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            var preBet = childSnapshot.val();
+
+            if(preBet.completed === true) {
+                global.init.db.ref('/preBetsAll/' + childSnapshot.key).update({
+                    "payable": 'true'
+                }).catch(error => {
+                    this.errorMessage = 'Error - ' + error.message
+                });
+                return true;
+            }
+        })
+    }).catch(error => {
+    this.errorMessage = 'Error - ' + error.message
+    });
+
+    return res.redirect(303, global.init.db.ref('/preBetsAll'));
     
 });
